5b11a3555d4ea324fc49bfcbbed18ed9db53d0f9
Adit Shah <aditshh@gmail.com>
2018-11-06 12:20:15 -0500

    Updated Comic Book Detail type to be strongly typed

--- ComicBookGallery/Models/ComicBook.cs
+++ ComicBookGallery/Models/ComicBook.cs
@@ -11,7 +11,6 @@ namespace ComicBookGallery.Models
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
-
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
